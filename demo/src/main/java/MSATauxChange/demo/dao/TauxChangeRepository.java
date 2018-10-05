f7522f6bb74c523f9845cbe321830a36f68fc995
Antoine2312 <anto.cou@hotmail.fr>
2018-10-05 22:46:57 +0200

    version finale corrigée

---
+++ demo/src/main/java/MSATauxChange/demo/dao/TauxChangeRepository.java
@@ -0,0 +1,17 @@
+package MSATauxChange.demo.dao;
+
+import MSATauxChange.demo.TauxChange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Repository
+public interface TauxChangeRepository extends JpaRepository<TauxChange, Integer> {
+
+   /* @Query("SELECT devise_source, devise_destination, taux, date FROM TauxChange t WHERE t.devise_source = :ds AND t.devise_destination = :dd")
+    TauxChange findByDevise_sourceAndDevise_destination(@Param("ds") String ds,@Param("dd") String dd);*/
+
+
+}
--- operation/src/main/java/MSAOperation/operation/dao/OperationChangeRepository.java
+++ operation/src/main/java/MSAOperation/operation/dao/OperationChangeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OperationChangeDAO extends JpaRepository<OperationChange, Integer> {
+public interface OperationChangeRepository extends JpaRepository<OperationChange, Integer> {
 
     @Query("SELECT id, devise_source, devise_destination, montant, date, taux FROM OperationChange o WHERE o.id = :i")
     OperationChange findByID(@Param("i") int i);
